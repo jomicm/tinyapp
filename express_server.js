d171cef7939112a255889fa61387da1fafc0aa04
jomicm <singularity.jmcm@gmail.com>
2019-09-30 18:31:04 +0000

    Implementing generateRandomString function

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,7 +32,27 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
 });
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
+});
+
+const generateRandomString = stringLength => {
+  const baseChar = [[65, 90], [97, 122], [48, 57]];
+  const result = [];
+  for (let i = 0; i < stringLength; i++) {
+    const randomChars = baseChar.map(x => String.fromCharCode(x[0] + getRandomTo(x[1] - x[0])));
+    result.push(randomChars[getRandomTo(2)]);
+  }
+  return result.join('');
+};
+const getRandomTo = number => Math.floor(Math.random() * (number + 1));
