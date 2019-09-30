11b2d09f936885714f612fca91b248efdba506bf
jomicm <singularity.jmcm@gmail.com>
2019-09-30 17:18:00 +0000

    Setting up an index view

--- express_server.js
+++ express_server.js
@@ -24,3 +24,8 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get('/url', (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
